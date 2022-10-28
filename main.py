1981b82a9f6d8ae20dd779c131dc8cd7a05114b7
Andy Athawale <andy.athawale@gmail.com>
2022-10-28 07:30:15 -0400

    Added code through step 3

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 ## Main code for running program
-from parsing import read_go_structure
+from parsing import *
+from annotation import *
 import networkx as nx
 
 g = nx.DiGraph()
-read_go_structure(g)
+read_go_structure(g, "go.obo.txt")
+human_interactions = read_go_annotations("goa_human.gaf")
+
+human_interactions_transferred = create_annotations(g, human_interactions)
+print("S")
