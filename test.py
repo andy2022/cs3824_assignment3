d03244c4dbde3a9c59035ef4326d21597a9318d5
Andy Athawale <andy.athawale@gmail.com>
2022-10-28 00:32:40 -0400

    Added more test cases

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import unittest
 import networkx as nx
 from parsing import *
+from annotation import *
 
 class tests(unittest.TestCase):
 
@@ -16,14 +17,32 @@ class tests(unittest.TestCase):
         g = nx.DiGraph()
         read_go_structure(g, "go.obo_test.txt")
 
-        self.assertEquals(len(g.nodes), 4)
-        self.assertEquals(g.nodes["GO:1"]["name"], "mitochondrion inheritance")
-        self.assertEquals(g.number_of_edges(), 3) # Check for 3 edges
+        self.assertEqual(len(g.nodes), 5) # Should only add 5 nodes
+        self.assertEqual(g.nodes["GO:1"]["name"], "mitochondrion inheritance")
+        self.assertEqual(g.number_of_edges(), 4) # Check for 4 edges
 
     def test_go_annotations(self):
         '''
-        Test case for smaller annotations parsing function.
+        Test case for annotations parsing function.
         '''
-        ret = read_go_annotations_test("goa_test.txt")
+        #ret = read_go_annotations("goa_human.gaf")
+        ret = read_go_annotations("goa_test.txt")
+
+        self.assertEqual(len(ret), 6)
+
+    def test_get_ancestors(self):
+        '''
+        Test case for getting ancestors for a node.
+        '''
+        g = nx.DiGraph()
+        read_go_structure(g, "go.obo_test.txt")
+        ancestors = ancestors_set(g, "GO:2")
+        self.assertEqual(ancestors.number_of_nodes(), 3)
+        self.assertEqual(ancestors.number_of_edges(), 2)
+
+        # Ancestors of node with no parents
+        ancestors = ancestors_set(g, "GO:1")
+        self.assertEqual(ancestors.number_of_nodes(), 0)
+        self.assertEqual(ancestors.number_of_edges(), 0)
 
 unittest.main()
